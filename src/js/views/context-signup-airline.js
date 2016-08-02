2f99fe5e81284f430ac5ed53b4529e57bcbeefa0
Pranil Singh <pranils@gmail.com>
2016-08-02 14:39:39 +1000

    Updated regex for email validation

--- src/js/views/context-signup-airline.js
+++ src/js/views/context-signup-airline.js
@@ -74,7 +74,7 @@ module.exports = View.extend({
 						required: true,
 						tests: [
 							function ( value ) { 
-								if ( !/[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g.test( value ) ) { 
+								if ( !/[A-Za-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?/g.test( value ) ) { 
 									return 'Please enter a valid email address.'; 
 								}
 							}
