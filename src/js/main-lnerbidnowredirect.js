2cf40f6f73bb980daa5ac8ccd9bb89e94c434196
Pranil Singh <pranil@seatfrog.com>
2018-07-03 16:19:01 +0100

    Adding LNER redirects as per Ben's ticket (VABFA-1094)

---
+++ src/js/main-lnerbidnowredirect.js
@@ -0,0 +1,51 @@
+
+
+//
+//	SEATFROG - LNER Bid Now Redirect
+//  Used in campaign email links sent by LNER
+//	
+
+
+'use strict';
+
+//	Dependencies
+var View = require('ampersand-view');
+
+//	App
+var FRONT = require('app');
+var MobileDetect = require('mobile-detect');
+var MD = new MobileDetect( window.navigator.userAgent );
+
+
+//
+//
+//
+
+
+var LNERBidNowRedirectView = View.extend({
+
+	initialize() {
+		this._os = MD.os();
+        var endpoint = 'http://seatfrog.com/lner.html';
+
+		if ( this._os === 'iOS' ) {
+			endpoint = FRONT.appStoreURLVTEC;
+		}
+		if ( this._os === 'AndroidOS' ) {
+			endpoint = FRONT.playStoreURLVTEC;
+		}
+		return window.location.replace( endpoint );
+	}
+});
+
+
+
+	
+/**
+ *	App Initialization
+ *  -------------------------------------
+ */
+
+FRONT.on('dom:start', () => {
+	FRONT.LNERBidNowRedirectView = new LNERBidNowRedirectView({ el: document.documentElement });
+});
